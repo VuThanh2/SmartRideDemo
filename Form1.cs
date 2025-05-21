83021e996f3785fb2e75a6d5785cf0dc34e8b2f0
VuThanh2 <chauthanhvu24122007@gmail.com>
2025-05-21 22:27:02 +0700

    Push message

--- Form1.cs
+++ Form1.cs
@@ -16,7 +16,8 @@ namespace SmartRideDemo
         private List<Driver> drivers = new List<Driver>();
         private List<Ride> rideHistory = new List<Ride>();
         private Random random = new Random();
-        
+        //Hiwwwwww
+
         public Form1()
         {
             InitializeComponent();
